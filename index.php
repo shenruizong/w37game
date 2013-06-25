fa0dfa7fe787102d3c3ac5e1b1678687a85ee067
disappeared <shenruizong@gmail.com>
2013-06-25 14:46:56 +0800

    添加初始代码

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+    define("APP_PATH","./App/");
+    define("APP_NAME","App");
+    define('ENGINE_NAME','Sae');
+    define("APP_DEBUG", true);
+    define('THINK_PATH','./ThinkPHP/');
+    require THINK_PATH.'ThinkPHP.php';
